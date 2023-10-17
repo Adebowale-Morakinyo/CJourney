c867a6208be3c193966c1e4da50e289b70efa1a1
Adebowale-Morakinyo <adebowalemorakinyo@gmail.com>
2023-10-17 19:56:41 +0100

    feat variables and data types: updating variable values

---
+++ chapter/updateVariableValue.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+
+    // the variable age with its original value
+    int age = 29;
+
+    // changing the value of age
+    // the new value will be 30
+    age = 30;
+}
