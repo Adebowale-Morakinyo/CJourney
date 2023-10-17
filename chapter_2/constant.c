27cbddfcf5ea70f156038e096906c0672d9d3427
Adebowale-Morakinyo <adebowalemorakinyo@gmail.com>
2023-10-17 20:24:29 +0100

    Constants in C

---
+++ chapter_2/constant.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void) {
+
+    const int LUCKY_NUM = 7;
+
+    printf("My lucky number is: %i\n", LUCKY_NUM);
+}
+
+/*
+#include <stdio.h>
+
+int main(void){
+    const int LUCKY_NUM = 7;
+
+    printf("My lucky number is: %i\n", LUCKY_NUM);
+
+    LUCKY_NUM = 13; // this will cause an error
+}
+*/
