4ca35b87c4e363a9ff175c768d07297159716eb0
Adebowale-Morakinyo <adebowalemorakinyo@gmail.com>
2023-12-02 22:44:55 +0100

    prototyping

--- Functions/functionReturn.c
+++ Functions/functionReturn.c
@@ -9,6 +9,22 @@ double cube(double num){
 int main(){
 
     printf("Answer: %f", cube(3.0));
-    
+
     return 0;
+}
+
+
+// prototyping, allowing you write other functions below main function
+double cube(double num);
+
+int main(){
+
+    printf("Answer: %f", cube(3.0));
+
+    return 0;
+}
+
+double cube(double num){
+    double result = num * num * num;
+    return result;
 }
