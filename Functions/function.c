df00a3f8d791ad2adcedbf7371fa9505577a926e
Adebowale-Morakinyo <adebowalemorakinyo@gmail.com>
2023-12-02 22:35:31 +0100

    Functions

---
+++ Functions/function.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    
+    sayHi();
+    sayHello("Mike");
+    return 0;
+}
+
+void sayHi(){
+    printf("Hi User");
+}
+
+// with parameters
+void sayHello(char name[], int age){
+    printf("Hello %s, you are %d\n", name, age);
+}
