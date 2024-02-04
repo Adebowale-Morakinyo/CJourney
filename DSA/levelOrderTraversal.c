5e2bbb68801b22ff5b08cbfb676c8651f98f4c38
Adebowale-Morakinyo <adebowalemorakinyo@gmail.com>
2024-02-04 20:35:00 +0100

    defined a structue for a tree node X funtion to create a new node.

--- DSA/levelOrderTraversal.c
+++ DSA/levelOrderTraversal.c
@@ -11,3 +11,21 @@
 left --> right
 
 */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// Define the structure for a tree node
+struct TreeNode {
+    int data;
+    struct TreeNode *left;
+    struct TreeNode *right;
+};
+
+// Function to create a new node
+struct TreeNode* createNode(int data) {
+    struct TreeNode* newNode = (struct TreeNode*)malloc(sizeof(struct TreeNode));
+    newNode->data = data;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
